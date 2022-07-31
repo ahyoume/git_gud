53fcaaf5007085350adf7ccaaf85d279e41adbcb
sidiousvic <sidiousvic@gmail.com>
2022-07-31 15:06:04 +0900

    add thoughts about broccoli

--- carrot.js
+++ carrot.js
@@ -2,3 +2,5 @@ I hate veggies.
 carrots are cool!!!
 eggplants are also cool
 don't forget about tomatoes! they are taaaastaaay
+
+you know what broccoli is difficult to think about. it looks fucking bad, but it taste good...
